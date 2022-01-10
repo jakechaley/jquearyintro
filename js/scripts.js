d3b0eec60f597b47bc630bb8e2a000a4b569f115
jakechaley <jakechaley@gmail.com>
2022-01-10 15:50:36 -0800

    ad elements using jQuery
    Co-Authored-By: Josh Tillinghast <jwtill@icloud.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("h1").click(function() {
+    alert("This is a heading.");
+    alert("I told you, THIS IS A HEADING!");
+  });
+  
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
